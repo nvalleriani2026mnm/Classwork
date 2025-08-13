d3a3bb2631b625d48c707ba98150c751b4ad3f47
nvalleriani2026mnm <nvalleriani2026@damien.edu>
2025-08-13 08:34:18 -1000

    Create FirstSubmission.java

---
+++ Unit1/FirstSubmission.java
@@ -0,0 +1,15 @@
+// Online Java - IDE, Code Editor, Compiler
+
+// Online Java is a quick and easy tool that helps you to build, compile, test your programs online.
+
+// Write your Java code here
+
+public class Main {
+  public static void main(String[] args) {
+   //Task 1: Print Hello World
+    System.out.print("Hello World");
+  
+  //Task 2: Print Hello World, go to next line, print Hello World
+    System.out.print("Hello World\nHello World");
+  }
+}
