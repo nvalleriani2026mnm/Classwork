9a06594c8adc9034f6d41ce9e02df324bdaef4b2
nvalleriani2026mnm <nvalleriani2026@damien.edu>
2025-08-29 18:18:02 -1000

    Create Modulus,CO,Casting.java

---
+++ Unit1/Modulus,CO,Casting.java
@@ -0,0 +1,39 @@
+
+public class ModulusCompOpsCast {
+
+	public static void main(String[] args) {
+		// Part 1: Modulus
+	     
+	     System.out.println("Line 1 = 1");
+	     System.out.println("Line 2 = 8");
+	     System.out.println("Line 3 = 0");
+	     System.out.println("Line 4 = 1");
+	     System.out.println("Line 5 = 4");
+	     System.out.println("Line 6 = 1");
+	     
+	   // Part 2: Compound Operators
+	     
+	     System.out.println("x becomes -1");
+	     System.out.println("y becomes 2");
+	     System.out.println("z now becomes 3");
+	     System.out.print("\n");
+	     
+	     System.out.println("a equals 4");
+	     System.out.println("b equals 2");
+	     System.out.println("c equals 3 ");
+	     System.out.print("\n\n");
+	     
+	     // Part 3: Casting
+	     
+	     int total = 5 + 7 + 2;
+	     total / 3;
+	    
+	     
+	     // The other options don't work because they try to change a double to a boolean
+	    
+
+		
+
+	}
+
+}
