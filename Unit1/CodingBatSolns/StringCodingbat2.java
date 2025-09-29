a066e2ab12b4cc178b864bd9104846b1dc722432
nvalleriani2026mnm <nvalleriani2026@damien.edu>
2025-09-28 21:54:29 -1000

    Create StringCodingbat2.java

---
+++ Unit1/CodingBatSolns/StringCodingbat2.java
@@ -0,0 +1,24 @@
+// makeOutWord
+public String makeOutWord(String out, String word) {
+  return out.substring(0, out.length() - 2) + word.substring(0, word.length()) + out.substring(out.length() - 2, out.length());
+  
+}
+
+// extraEnd
+public String extraEnd(String str) {
+  return str.substring(str.length() - 2) + str.substring(str.length() - 2) + str.substring(str.length() - 2);
+}
+
+// firstTwo
+public String firstTwo(String str) {
+  if (str.length() < 2) {
+    return str;
+  }
+  
+  return str.substring(0, 2);
+}
+
+// withoutEnd
+public String withoutEnd(String str) {
+  return str.substring(1, str.length() - 1);
+}
