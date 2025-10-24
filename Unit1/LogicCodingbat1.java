ad6e13ceb9493f26e99c2daf982faa38b5d24d0f
nvalleriani2026mnm <nvalleriani2026@damien.edu>
2025-10-24 09:55:33 -1000

    Update LogicCodingbat1.java

--- Unit1/LogicCodingbat1.java
+++ Unit1/LogicCodingbat1.java
@@ -96,4 +96,35 @@ public boolean old35(int n) {
   }
 }
 
-//???
+//alarmClock
+public String alarmClock(int day, boolean vacation) {
+  if (vacation == true) {
+    if ((day == 0) || (day == 6)) {
+      return "off";
+    }
+    else {
+      return "10:00";
+    }
+  }
+  else if ((day == 0) || (day == 6)) {
+    return "10:00";
+  }
+  else {
+    return "7:00";
+  }
+}
+
+//in1To10
+public boolean in1To10(int n, boolean outsideMode) {
+  if (outsideMode == false) {
+    if ((n >= 1) && (n <= 10)) {
+      return true;
+    }
+  }
+  else if ((n <= 1) || (n >= 10)) {
+    return true;
+  }
+  return false;
+}
+
+
