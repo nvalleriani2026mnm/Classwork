c0676caf1f012753c72c6e9f9f9d498ec7eb6ba1
nvalleriani2026mnm <nvalleriani2026@damien.edu>
2025-10-24 08:13:09 -1000

    Update LogicCodingbat1.java

--- Unit1/LogicCodingbat1.java
+++ Unit1/LogicCodingbat1.java
@@ -86,4 +86,14 @@ public int sortaSum(int a, int b) {
   }
 }
 
+//old35
+public boolean old35(int n) {
+  if ((n % 3 == 0 || n % 5 == 0) && !(n % 3 == 0 && n % 5 == 0)) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
 
+//???
