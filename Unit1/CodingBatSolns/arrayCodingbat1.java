0fa8424e9fd2a2e1caf9e9dbb19a80511a368a66
nvalleriani2026mnm <nvalleriani2026@damien.edu>
2026-01-16 13:01:04 -1000

    Create arrayCodingbat1.java

---
+++ Unit1/CodingBatSolns/arrayCodingbat1.java
@@ -0,0 +1,59 @@
+// firstLast6
+public boolean firstLast6(int[] nums) {
+  if(nums[0] == 6 || nums[nums.length-1] == 6) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+// sameFirstLast
+public boolean sameFirstLast(int[] nums) {
+  if(nums.length > 0) {
+    if(nums[0] == nums[nums.length-1]) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+  else {
+    return false;
+  }
+}
+
+// makePi
+public int[] makePi() {
+  int[] piNums = {3, 1, 4};
+  return piNums;
+}
+  
+// commonEnd
+public boolean commonEnd(int[] a, int[] b) {
+  if((a[0] == b[0]) || (a[a.length-1] == b[b.length-1])) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+// sum3
+public int sum3(int[] nums) {
+  int sum = 0;
+  for(int i = 0; i < nums.length; i++) {
+    sum += nums[i];
+  }
+  return sum;
+}
+
+
+
+
+
+
+
+
+
+
